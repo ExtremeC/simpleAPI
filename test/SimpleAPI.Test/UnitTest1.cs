11ccb6da44fba007efb100168c55523a00964480
ahmed <ahmedsh9992g@gmail.com>
2020-06-15 14:27:02 +0200

    new start

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -2,19 +2,21 @@ using System;
 using Xunit;
 using SimpleAPI.Controllers;
 
+
 namespace SimpleAPI.Test
 {
     public class UnitTest1
     {
-         WeatherForecastController controller = new WeatherForecastController();
-         [Fact]
-         public void getReturnsMyName()
-         {
-         var returnValue = controller.Get(1);
-         Assert.Equal("ahmed",returnValue.Value);
-         }
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Les Jackson", returnValue.Value);
+        }
 
-    
+
+
         [Fact]
         public void Test1()
         {
